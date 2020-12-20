cb1e1ef6729c1c88e33378e9368f5cc8f05562e4
Kaden Slater <developerInProgress9000@gmail.com>
2020-12-20 12:33:17 -0600

    Attempted Issue #1

--- HelloWorldWebComponent/hwwc.js
+++ HelloWorldWebComponent/hwwc.js
@@ -1,3 +1,6 @@
+
+var gbwIsRegistered = false;
+
 // ES6 class
 class HelloWorld extends HTMLParagraphElement {
   // constructor is called when the element is displayed
@@ -11,13 +14,44 @@ class HelloWorld extends HTMLParagraphElement {
     text.textContent = "Hello World";
     // insert our created element into our shadow DOM, causing it to appear
     shadow.appendChild(text);
-
-    console.log("HelloWorld constructed");
   }
 }
-  
+
+
+// ES6 class
+class GoodbyeWorld extends HTMLParagraphElement {
+  // constructor is called when the element is displayed
+  constructor() {
+    super();
+    // create a shadow dom
+    const shadow = this.attachShadow({ mode: "closed" });
+    // create a span element
+    const text = document.createElement("span");
+    // set the content to 'Goodbye World'
+    text.textContent = "Goodbye World";
+    // insert our created element into our shadow DOM, causing it to appear
+    shadow.appendChild(text);
+  }
+}
+
+
+function registerHelloWorld() {
+  customElements.define("hello-world", HelloWorld, { extends: "p" });
+}
+
+function registerGoodbyeWorld() {
+  if(!gbwIsRegistered) {
+    customElements.define("goodbye-world", GoodbyeWorld, { extends: "p" });
+
+    gbwIsRegistered = true;
+  }
+}
+
+
 // make sure that the <hello-world></hello-world>
 // or simply <hello-world /> is recognised as this element
 window.onload = function() {
-  customElements.define("hello-world", HelloWorld, { extends: "p" });
+  registerHelloWorld();
+
+  
 }
