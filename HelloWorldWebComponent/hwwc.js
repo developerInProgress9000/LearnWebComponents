89f91887d30e08e5e82e42dee7e31bf04bf6804c
Kaden Slater <developerInProgress9000@gmail.com>
2020-12-20 11:29:21 -0600

    Added Window Onload Event Listener for Cust Elem Registry

--- HelloWorldWebComponent/hwwc.js
+++ HelloWorldWebComponent/hwwc.js
@@ -1,19 +1,23 @@
 // ES6 class
 class HelloWorld extends HTMLParagraphElement {
-    // constructor is called when the element is displayed
-    constructor() {
-      super();
-      // create a shadow dom
-      const shadow = this.attachShadow({ mode: "closed" });
-      // create a span element
-      const text = document.createElement("span");
-      // set the content to 'Hello World'
-      text.textContent = "Hello World";
-      // insert our created element into our shadow DOM, causing it to appear
-      shadow.appendChild(text);
-    }
+  // constructor is called when the element is displayed
+  constructor() {
+    super();
+    // create a shadow dom
+    const shadow = this.attachShadow({ mode: "closed" });
+    // create a span element
+    const text = document.createElement("span");
+    // set the content to 'Hello World'
+    text.textContent = "Hello World";
+    // insert our created element into our shadow DOM, causing it to appear
+    shadow.appendChild(text);
+
+    console.log("HelloWorld constructed");
   }
+}
   
-  // make sure that the <hello-world></hello-world>
-  // or simply <hello-world /> is recognised as this element
+// make sure that the <hello-world></hello-world>
+// or simply <hello-world /> is recognised as this element
+window.onload = function() {
   customElements.define("hello-world", HelloWorld, { extends: "p" });
+}
